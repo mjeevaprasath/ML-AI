5ec6cecf267a949a58f0b920777368e12bfc582d
Your Name <your@email.com>
2026-05-07 16:04:37 +0530

    removed pygame

--- ml_backend/ml_backend/settings.py
+++ ml_backend/ml_backend/settings.py
@@ -117,12 +117,12 @@ STATICFILES_DIRS = [
     BASE_DIR / "ai_ml_assistant" / "static"
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://127.0.0.1:8000",
-    "http://localhost:8000",
+    "https://*.onrender.com",
 ]
 
 # =====================
