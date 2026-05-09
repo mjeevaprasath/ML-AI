82d9cd8ae8b0af206c6e3e3916b5b4b7742ad57a
Your Name <your@email.com>
2026-05-09 17:33:38 +0530

    fixed internal server error

--- ml_backend/ml_backend/urls.py
+++ ml_backend/ml_backend/urls.py
@@ -2,10 +2,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('ai_ml_assistant.urls')),
+
+    # Test homepage
+    path('', lambda request: HttpResponse("ML AI Backend Running Successfully")),
+
+    # App URLs
+    path('api/', include('ai_ml_assistant.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
